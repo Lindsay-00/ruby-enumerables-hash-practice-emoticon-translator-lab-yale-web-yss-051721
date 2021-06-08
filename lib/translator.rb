07f967f89127ca1b8ed2102df8303b63f72684e6
Lindsay Chen <lindsay.chen@yale.edu>
2021-06-08 13:03:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,43 @@
 # require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+
+def load_library(path)
+  key = ''
+  emoticons = { key => {:english => "", :japanese => ""}}
+  YAML.load_file(path).each do |meaning, describe|
+     eng, jan = describe
+     emoticons[key] = meaning
+     
+     emoticons[key][:english] = eng
+     emoticons[key][:japanese] = jan
+  end
+  emoticons
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+
+#p load_library './lib/emoticons.yml'
+
+
+def get_japanese_emoticon(path, emoticon)
+  emoticons = load_library(path) 
+  result = emoticons[:japanese][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+
+def get_english_meaning(path, emoticon)
+  emoticons = load_library(path)
+  result = emoticons[:English][emoticon] 
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
